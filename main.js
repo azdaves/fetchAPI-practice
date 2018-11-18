c1b40543e4ed2252c76c44a4804f9d474e9eba42
Dave Schilling <ds@Daves-MacBook-Pro.local>
2018-11-18 16:55:52 -0700

    adds json example

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 document.getElementById("getText").addEventListener("click", getText);
+document.getElementById("getFighters").addEventListener("click", getFighters);
 
 function getText() {
 
@@ -7,5 +8,27 @@ function getText() {
     .then((res) => res.text())
     .then((data) => {
         document.getElementById('output').innerHTML = data;
-    });
+    })
+    .catch((err) => console.log(err))
 }
+
+function getFighters() {
+
+
+    fetch("fighters.json")
+      .then((res) => res.json())
+      .then((data) => {
+          let output = '<h2>Fighters</h2>';
+          data.forEach(function(fighter){
+              output += `
+              <ul>
+              <li>ID: ${fighter.id}</li>
+              <li>NAME: ${fighter.name}</li>
+              <li>EMAIL: ${fighter.email}</li>
+              </ul>
+              `;
+          });
+          document.getElementById('output').innerHTML = output;
+      })
+      
+  }
